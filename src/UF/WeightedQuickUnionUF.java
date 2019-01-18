9289b45b972c1ee0bf64d4a63c683e6db74e8e10
AHL-09 <43703854+AHL-09@users.noreply.github.com>
2019-01-18 06:22:38 +0330

    Initial commit

---
+++ src/UF/WeightedQuickUnionUF.java
@@ -0,0 +1,124 @@
+package UF;
+
+public class WeightedQuickUnionUF {
+    private int[] id;  // id[i] = parent of i
+    private int[] sz;  // size[i] = number of sites in subtree rooted at i
+    private int count; // number of components
+
+    /**
+     * Initializes an empty union–find data structure with {@code n} sites
+     * {@code 0} through {@code n-1}. Each site is initially in its own
+     * component.
+     *
+     * @param N the number of sites
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public WeightedQuickUnionUF(int N) {
+        count = N;
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+
+
+    }
+
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input,
+     * where each integer represents some object;
+     * if the sites are in different components, merge the two components
+     * and print the pair to standard output.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.getCount() + " components");
+    }
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between {@code 1} and {@code n})
+     */
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * Returns true if the the two sites are in the same component.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+     * {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *                                  both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Returns the component identifier for the component containing site {@code p}.
+     *
+     * @param p the integer representing one object
+     * @return the component identifier for the component containing site {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     */
+    private int find(int p) {
+        validate(p);
+        // Follow links to find a root.
+        while (p != id[p]) p = id[p];
+        return p;
+    }
+
+    //validate that p is a valid index
+    private void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n - 1));
+        }
+    }
+
+    /**
+     * Merges the component containing site {@code p} with the
+     * the component containing site {@code q}.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @throws IllegalArgumentException unless
+     *                                  both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public void union(int p, int q) {
+        // Put p and q into the same component.
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        // Nothing to do if p and q are already
+        // in the same component.
+
+        if (rootP == rootQ) return;
+
+        // Make smaller root point to larger one.
+
+        if (sz[rootP] < sz[rootQ]) {
+            id[rootP] = rootQ;
+            sz[rootQ] += sz[rootP];
+        } else {
+            id[rootQ] = rootP;
+            sz[rootP] += sz[rootQ];
+        }
+        count--;
+    }
+}
