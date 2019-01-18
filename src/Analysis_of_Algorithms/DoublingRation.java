949a778fc42d173f07a1222de4625ddda1c4d1f1
AHL-09 <43703854+AHL-09@users.noreply.github.com>
2019-01-18 10:49:27 +0330

    Initial commit

---
+++ src/Analysis_of_Algorithms/DoublingRation.java
@@ -0,0 +1,31 @@
+package Analysis_of_Algorithms;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+
+public class DoublingRation {
+    public static double timeTrial(int N) {
+// Time ThreeSum.count() for N random 6-digit ints.
+        int MAX = 1000000;
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++)
+            a[i] = StdRandom.uniform(-MAX, MAX);
+        Stopwatch timer = new Stopwatch();
+        int cnt = ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+    public static void main(String[] args) { // Print table of running times.
+        double prev = timeTrial(125);
+        for (int N = 250; true; N += N) { // Print time for problem size N.
+            double time = timeTrial(N);
+            StdOut.printf("%6d %7.1f ", N, time);
+            StdOut.printf("%5.1f\n", time / prev);
+            prev = time;
+        }
+    }
+}
+
+
